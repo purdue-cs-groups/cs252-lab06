bb3f92a43801a17729707b23da67cc822b3f9df3
Matt McCormick <mbmccormick@gmail.com>
2011-11-17 16:13:10 -0500

    Fixing tabs and adding user to Directory List collection on server.

--- Server/DirectoryServerConnection.java
+++ Server/DirectoryServerConnection.java
@@ -25,58 +25,62 @@ public class DirectoryServerConnection implements Runnable
 
             // read each line
             String line;
-			String userName, addIP;
-			String sendIP;
-			String acceptIP;
-			String hangupIP;			
-			
+            String userName, addIP;
+            String sendIP;
+            String acceptIP;
+            String hangupIP;            
+            
             while ((line = br.readLine()) != null)
             {
-				// AddUser request
-                if (line.startsWith("<AddUser>")) {
-					userName = br.readLine();
-					userName = userName.substring(10, userName.length() - 11);
+                // AddUser request
+                if (line.startsWith("<AddUser>"))
+                {
+                    userName = br.readLine();
+                    userName = userName.substring(10, userName.length() - 11);
 
-					addIP = br.readLine();
-					addIP = addIP.substring(11, addIP.length() - 12);
-
-					System.out.println("\nAddUser Request");
-					System.out.println("---------------");
-					System.out.println("New user:   " + userName);
-					System.out.println("IP Address: " + addIP);
-				}
-				// GetDirectory request
-				else if (line.startsWith("<GetDirectory>")) {
-					System.out.println("\nGetDirectory Request");
-				}	
-				// SendCall request
-				else if (line.startsWith("<SendCall>")) {
-					sendIP = br.readLine();
-					sendIP = sendIP.substring(11, sendIP.length() - 12);
-					System.out.println("\nSendCall Request");
-					System.out.println("----------------");
-					System.out.println("IP Address: " + sendIP);
-				}
-				// AcceptCall request
-				else if (line.startsWith("<AcceptCall>")) {
-					acceptIP = br.readLine();
-					acceptIP = acceptIP.substring(11, acceptIP.length() - 12);
-					System.out.println("\nAcceptCall Request");
-					System.out.println("------------------");
-					System.out.println("IP Address: " + acceptIP);
-				}
-				// Hangup request
-				else if (line.startsWith("<Hangup>")) {
-					hangupIP = br.readLine();
-					hangupIP = hangupIP.substring(11, hangupIP.length() - 12);
-					System.out.println("\nHangup Request");
-					System.out.println("--------------");
-					System.out.println("IP Address: " + hangupIP);
-				}
-				// Something else
-				else {
-					// ignore
-				}
+                    addIP = br.readLine();
+                    addIP = addIP.substring(11, addIP.length() - 12);
+                    
+                    User u = new User(username, addIP);
+                    _host._directory.add(u);
+                    
+                    System.out.println("\nAddUser Request");
+                    System.out.println("---------------");
+                    System.out.println("New user:   " + userName);
+                    System.out.println("IP Address: " + addIP);
+                }
+                // GetDirectory request
+                else if (line.startsWith("<GetDirectory>")) {
+                    System.out.println("\nGetDirectory Request");
+                }    
+                // SendCall request
+                else if (line.startsWith("<SendCall>")) {
+                    sendIP = br.readLine();
+                    sendIP = sendIP.substring(11, sendIP.length() - 12);
+                    System.out.println("\nSendCall Request");
+                    System.out.println("----------------");
+                    System.out.println("IP Address: " + sendIP);
+                }
+                // AcceptCall request
+                else if (line.startsWith("<AcceptCall>")) {
+                    acceptIP = br.readLine();
+                    acceptIP = acceptIP.substring(11, acceptIP.length() - 12);
+                    System.out.println("\nAcceptCall Request");
+                    System.out.println("------------------");
+                    System.out.println("IP Address: " + acceptIP);
+                }
+                // Hangup request
+                else if (line.startsWith("<Hangup>")) {
+                    hangupIP = br.readLine();
+                    hangupIP = hangupIP.substring(11, hangupIP.length() - 12);
+                    System.out.println("\nHangup Request");
+                    System.out.println("--------------");
+                    System.out.println("IP Address: " + hangupIP);
+                }
+                // Something else
+                else {
+                    // ignore
+                }
             }
         }
         catch (IOException ex)
