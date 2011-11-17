c3af413fbea04b23a80737fa9163fd38a41e6229
Matt McCormick <mbmccormick@gmail.com>
2011-11-17 14:45:33 -0500

    Adding pseudo code for RingerServer and VoiceServer.

--- Server/Server.java
+++ Server/Server.java
@@ -3,18 +3,27 @@ import java.net.*;
 
 public class Server
 {
-    protected static int SERVER_PORT = 4444;
-
     public static void main(String[] args)
     {
         System.out.println("Starting server...");
         
-        // create a new instance of the server
-        DirectoryServer ds = new DirectoryServer(SERVER_PORT);
+        // create a new instance of the directory server
+        DirectoryServer ds = new DirectoryServer(6900); 
         
-        // launch a new thread for this server
-        Thread th = new Thread(ds);        
-        th.start();
+        Thread t1 = new Thread(ds);        
+        t1.start();
+        
+        // create a new instance of the ringer server
+        // TODO: RingerServer rs = new RingerServer(6901);    
+        
+        // TODO: Thread t2 = new Thread(rs);        
+        // TODO: t2.start();
+        
+        // create a new instance of the voice server
+        // TODO: VoiceServer vs = new VoiceServer(6902);
+        
+        // TODO: Thread t3 = new Thread(vs);        
+        // TODO: t3.start();
     }
 }
 
