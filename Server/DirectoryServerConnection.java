84db4ca4428506fc0a77352133fd4707951d055b
Matt McCormick <mbmccormick@gmail.com>
2011-11-18 09:04:25 -0500

    Removing out.close() as it closes the connection.

--- Server/DirectoryServerConnection.java
+++ Server/DirectoryServerConnection.java
@@ -68,8 +68,6 @@ public class DirectoryServerConnection implements Runnable
                         out.println("</User>");
                     }
                     out.println("</Directory>");
-                    
-                    out.close();
                 }    
                 // SendCall request
                 else if (line.startsWith("<SendCall>")) {
