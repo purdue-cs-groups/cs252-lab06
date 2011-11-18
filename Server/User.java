c20d292cbe3a4cfee1348a790d4f4f521294c578
Matt McCormick <mbmccormick@gmail.com>
2011-11-18 09:56:03 -0500

    Adding status property to User object.

--- Server/User.java
+++ Server/User.java
@@ -6,11 +6,13 @@ public class User
 {
     private String _username;
     private String _ipAddress;
+    private String _status;
     
     public User(String username, String ipAddress)
     {
         _username = username;
         _ipAddress = ipAddress;
+        _status = "Available";
     }
     
     public String getUsername()
@@ -21,6 +23,16 @@ public class User
     public String getIPAddress()
     {
         return _ipAddress;
+    }
+    
+    public String getStatus()
+    {
+        return _status;
+    }
+    
+    public void setStatus(String value)
+    {
+        _status = value;
     }
 }
 
