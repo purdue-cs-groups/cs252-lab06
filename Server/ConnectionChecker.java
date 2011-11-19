505b64ce4ea353dc56f3b5beac2c77f4d1d2ea5a
Matt McCormick <mbmccormick@gmail.com>
2011-11-18 20:22:00 -0500

    Replacing tabs with spaces.

--- Server/ConnectionChecker.java
+++ Server/ConnectionChecker.java
@@ -4,57 +4,57 @@ import java.net.*;
 
 public class ConnectionChecker implements Runnable
 {
-	private DirectoryServer _ds = null;
-	private int _sleepTime = 5000; // how often to check in ms
-	private int _checkTime = 5000; // whatever is considered old in ms
+    private DirectoryServer _ds = null;
+    private int _sleepTime = 5000; // how often to check in ms
+    private int _checkTime = 5000; // whatever is considered old in ms
 
-	ConnectionChecker (DirectoryServer ds)
-	{
-		_ds = ds;
-	}
+    ConnectionChecker (DirectoryServer ds)
+    {
+        _ds = ds;
+    }
 
-	public void run()
-	{
-		System.out.println("ConnectionChecker is in action...");
+    public void run()
+    {
+        System.out.println("ConnectionChecker is in action...");
 
-		while (true) {
-			System.out.println("Checking for stale threads...");
-			Long currTime = Calendar.getInstance().getTimeInMillis();
-			Long timeDiff = new Long(0);
+        while (true) {
+            System.out.println("Checking for stale threads...");
+            Long currTime = Calendar.getInstance().getTimeInMillis();
+            Long timeDiff = new Long(0);
 
-			for (Map.Entry<String, DirectoryServerConnection> e : _ds._connections.entrySet())
+            for (Map.Entry<String, DirectoryServerConnection> e : _ds._connections.entrySet())
             {
-				timeDiff = currTime - e.getValue()._lastKeepAliveTime;
+                timeDiff = currTime - e.getValue()._lastKeepAliveTime;
 
-				if (timeDiff > _checkTime)
-				{
-					// kill connection
-					try
-					{
-						e.getValue()._clientSocket.close();
-					}
-					catch (IOException ex)
-					{
-						// TODO: handle this exception			
-					}
-					
-					System.out.println(e.getKey() + " needs to die.");
-				}
-				else
-				{
-					// connection is active
-					System.out.println(e.getKey() + " can live.");
-				}
-			}
+                if (timeDiff > _checkTime)
+                {
+                    // kill connection
+                    try
+                    {
+                        e.getValue()._clientSocket.close();
+                    }
+                    catch (IOException ex)
+                    {
+                        // TODO: handle this exception            
+                    }
+                    
+                    System.out.println(e.getKey() + " needs to die.");
+                }
+                else
+                {
+                    // connection is active
+                    System.out.println(e.getKey() + " can live.");
+                }
+            }
 
-			try
-			{
-				Thread.sleep(_sleepTime);	
-			}
-			catch (InterruptedException e)
-			{
-				// TODO: handle this exception
-			}
-		}
-	}
+            try
+            {
+                Thread.sleep(_sleepTime);    
+            }
+            catch (InterruptedException e)
+            {
+                // TODO: handle this exception
+            }
+        }
+    }
 }
