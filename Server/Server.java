025517f15c44d0fcf829b51254bc99f7e0ce6285
Matt McCormick <mbmccormick@gmail.com>
2011-11-20 13:50:31 -0500

    Replacing tabs with spaces.

--- Server/Server.java
+++ Server/Server.java
@@ -11,14 +11,14 @@ public class Server
         // create a new instance of the directory server
         DirectoryServer ds = new DirectoryServer(6900); 
 		
-		Thread t1 = new Thread(ds);        
+        Thread t1 = new Thread(ds);        
         t1.start();
 
         // create a new instance of the heartbeat monitor
-		ConnectionChecker cs = new ConnectionChecker(ds);
+        ConnectionChecker cs = new ConnectionChecker(ds);
         
         Thread connectionChecker = new Thread(cs);
-		connectionChecker.start();
+        connectionChecker.start();
         
         // create a new instance of the voice server
         VoiceServer vs = new VoiceServer(6901);
